d5ef114e9d4c2dd77be4163dd3f1c0b2530792a1
gerardo0579 <gerardo0579@hotmail.com>
2022-05-25 11:49:01 -0500

    Fix typo

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,18 @@
 import React, { FC } from 'react'
-import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 import { Main } from './AppComponents/Main/Main'
 
 export const AppRouter: FC = () => (
   <Router>
     <Switch>
-      <Route exact path="/:username" component={Main} />>
-      <Route path='*'>
-        <Redirect to='/yknx4' />
+      <Route exact path="/:username" component={Main} />
+      <Route path="*">
+        <Redirect to="/yknx4" />
       </Route>
     </Switch>
   </Router>
